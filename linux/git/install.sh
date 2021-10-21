937e58f83436e0fe7ce0e4945c6396d525053e96
Estevao Francisco Fay <estevao@ciandt.com>
2021-10-21 13:26:51 -0300

    Add krew and update git

--- linux/git/install.sh
+++ linux/git/install.sh
@@ -8,3 +8,6 @@ sudo apt-get install git
 
 git config --global --edit
 git config --global pull.rebase true
+
+git config --global user.name "Estêvão Francisco Fáy"
+git config --global user.email "estevao@ciandt.com"
